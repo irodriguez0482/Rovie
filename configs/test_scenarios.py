75e1fec3448bd0d3319be22ac744db45a112e674
Eva <rod.ive1130@gmail.com>
2025-03-29 20:23:44 -0400

    Having multiple scenarios.

--- configs/test_scenarios.py
+++ configs/test_scenarios.py
@@ -1,17 +1,32 @@
-# test_scenarios.py
+# config/test_scenarios.py
 
-# Time (in seconds) since start of test run when events should occur
-SCENARIO = {
-    "name": "Multiple obstacles and E-Stop",
-
-    "force_button_press_times": [
-        5,   # First obstacle
-        10,  # Second obstacle
-        15,  # Third obstacle
-    ],
-
-    "estop_trigger_time": 18,  # Simulated emergency shutdown
-
-    "gps_fail_times": [7, 14],  # GPS drops out for a moment at these seconds
-
+SCENARIOS = {
+    "default": {
+        "name": "Default clean run",
+        "force_button_press_times": [],
+        "estop_trigger_time": None,
+        "gps_fail_times": []
+    },
+    "obstacle_map_test": {
+        "name": "Map Grid Test",
+        "force_button_press_times": [4, 10],
+        "estop_trigger_time": None,
+        "gps_fail_times": []
+    },
+    "estop_midway": {
+        "name": "Emergency Stop Test",
+        "force_button_press_times": [],
+        "estop_trigger_time": 12,
+        "gps_fail_times": []
+    },
+    "gps_dropouts": {
+        "name": "GPS Signal Drop Test",
+        "force_button_press_times": [],
+        "estop_trigger_time": None,
+        "gps_fail_times": [5, 15]
+    }
 }
+
+# Choose the active one here:
+ACTIVE_SCENARIO_NAME = "obstacle_map_test"
+SCENARIO = SCENARIOS[ACTIVE_SCENARIO_NAME]
