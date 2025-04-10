130fdb824ae089abb40336a6e091d02a56d8d681
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2025-04-10 13:03:13 -0400

    started demo code

---
+++ testing/rover_demo.py
@@ -0,0 +1,31 @@
+import time
+from core import obstacle_avoidance, arm_control_logic
+from hardware import gps, motors, force_button, estop
+from utils.coordinate_utils import haversine_distance
+from utils import map_tracker
+
+def run_demo_mode():
+    try:
+        motors.init_serial()
+        motors.stop_all()
+        motors.drive_forward()
+        time.sleep(1)
+        motors.drive_backward()
+        time.sleep(1)
+        motors.turn_left()
+        time.sleep(1)
+        motors.turn_right()
+        time.sleep(1)
+        motors.stop_drive()
+        arm_control_logic.update_arm_state()
+        time.sleep(1)
+        arm_control_logic.update_arm_state("reroute")
+        time.sleep(1)
+        motors.stop_all()
+
+    except KeyboardInterrupt:
+        print("[DEMO] Interrupted by user.")
+    finally:
+        motors.cleanup()
+        force_button.cleanup()
+        estop.cleanup()
