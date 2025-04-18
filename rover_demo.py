4415e32f597e54c99dc6e9868be28c001b56f8f8
Eva <rod.ive1130@gmail.com>
2025-04-18 13:53:06 -0400

    Adding in keybinds?

--- rover_demo.py
+++ rover_demo.py
@@ -1,31 +1,72 @@
 import time
+import keyboard  # pip install keyboard
 from core import obstacle_avoidance, arm_control_logic
 from hardware import gps, motors, force_button, estop
 from utils.coordinate_utils import haversine_distance
 from utils import map_tracker
 
-def run_demo_mode():
-    runTime = 6
+def run_keyboard_demo():
+    print("\n[DEMO] Press a key to activate a function. Press 'q' to quit.\n")
+    print("""
+    Controls:
+    w - Drive Forward
+    s - Drive Backward
+    a - Turn Left
+    d - Turn Right
+    x - Stop All
+    u - Arm Up
+    j - Arm Down
+    v - Vibration On
+    b - Vibration Off
+    q - Quit
+    """)
+
+    motors.init_serial()
+    motors.stop_all()
+
     try:
-        motors.init_serial()
-        motors.stop_all()
-        time.sleep(runTime)
-        motors.drive_forward()
-        time.sleep(runTime)
-        motors.drive_backward()
-        time.sleep(runTime)
-        motors.turn_left()
-        time.sleep(runTime)
-        motors.turn_right()
-        time.sleep(runTime)
-        motors.stop_drive()
-        # arm_control_logic.update_arm_state()
-        motors.vibration_on()
-        time.sleep(runTime)
-        # arm_control_logic.update_arm_state("reroute")
-        motors.vibration_off()
-        time.sleep(runTime)
-        motors.stop_all()
+        while True:
+            if keyboard.is_pressed('w'):
+                print("[DEMO] Driving forward")
+                motors.drive_forward()
+                time.sleep(0.5)
+
+            elif keyboard.is_pressed('s'):
+                print("[DEMO] Driving backward")
+                motors.drive_backward()
+                time.sleep(0.5)
+
+            elif keyboard.is_pressed('a'):
+                print("[DEMO] Turning left")
+                motors.turn_left()
+                time.sleep(0.5)
+
+            elif keyboard.is_pressed('d'):
+                print("[DEMO] Turning right")
+                motors.turn_right()
+                time.sleep(0.5)
+
+            elif keyboard.is_pressed('x'):
+                print("[DEMO] Stopping all motors")
+                motors.stop_all()
+                time.sleep(0.5)
+
+
+            elif keyboard.is_pressed('v'):
+                print("[DEMO] Vibration ON")
+                motors.vibration_on()
+                time.sleep(0.5)
+
+            elif keyboard.is_pressed('b'):
+                print("[DEMO] Vibration OFF")
+                motors.vibration_off()
+                time.sleep(0.5)
+
+            elif keyboard.is_pressed('q'):
+                print("[DEMO] Quitting demo mode.")
+                break
+
+            time.sleep(0.1)  # Prevent CPU overuse
 
     except KeyboardInterrupt:
         print("[DEMO] Interrupted by user.")
@@ -33,10 +74,7 @@ def run_demo_mode():
         motors.cleanup()
         force_button.cleanup()
         estop.cleanup()
-        
-try:
-    run_demo_mode()
-except KeyboardInterrupt:
-    print("\n[DEMO] Shutdown via KeyboardInterrupt.")
-finally:
-    motors.cleanup()
+
+# Run the keyboard demo
+if __name__ == "__main__":
+    run_keyboard_demo()
