5ce61e6ca5f57065f770d4b069c42237d25f0446
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2025-04-22 16:53:28 -0400

    removed cleanup

--- rover_demo_cycle.py
+++ rover_demo_cycle.py
@@ -5,38 +5,24 @@ from utils.coordinate_utils import haversine_distance
 from utils import map_tracker
 
 def run_demo_mode():
-    runTime = 6
-    try:
-        motors.init_serial()
-        motors.stop_all()
-        time.sleep(runTime)
-        motors.drive_forward()
-        time.sleep(runTime)
-        motors.drive_backward()
-        time.sleep(runTime)
-        motors.turn_left()
-        time.sleep(runTime)
-        motors.turn_right()
-        time.sleep(runTime)
-        motors.stop_drive()
-        arm.arm_up()
-        motors.vibration_on()
-        time.sleep(runTime)
-        arm.arm_down()
-        motors.vibration_off()
-        time.sleep(runTime)
-        motors.stop_all()
-
-    except KeyboardInterrupt:
-        print("[DEMO] Interrupted by user.")
-    finally:
-        motors.cleanup()
-        force_button.cleanup()
-        estop.cleanup()
-        
-# try:
-#     run_demo_mode()
-# except KeyboardInterrupt:
-#     print("\n[DEMO] Shutdown via KeyboardInterrupt.")
-# finally:
-#     motors.cleanup()
+    runTime = 3
+    
+    motors.init_serial()
+    motors.stop_all()
+    time.sleep(0.5)
+    motors.drive_forward()
+    time.sleep(runTime)
+    motors.drive_backward()
+    time.sleep(runTime)
+    motors.turn_left()
+    time.sleep(runTime)
+    motors.turn_right()
+    time.sleep(runTime)
+    motors.stop_drive()
+    arm.arm_up()
+    motors.vibration_on()
+    time.sleep(runTime)
+    arm.arm_down()
+    motors.vibration_off()
+    time.sleep(runTime)
+    motors.stop_all()
