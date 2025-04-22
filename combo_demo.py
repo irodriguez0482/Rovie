36ceb3a4c5b3d474024b17f6c23c8c6db5a94a9e
CatherineHolt <113729707+CatherineHolt@users.noreply.github.com>
2025-04-22 17:00:58 -0400

    Reprinting keyboard message

--- combo_demo.py
+++ combo_demo.py
@@ -96,7 +96,21 @@ def run_keyboard_Demo():
             elif is_Pressed():
                 print("Rover Demo Started!")
                 run_demo_mode()
-                time.sleep(0.5)
+                time.sleep(30)
+                print("\n[DEMO] Press a key to activate a function. Press 'q' to quit.\n")
+                print("""
+                Controls:
+                w - Drive Forward
+                s - Drive Backward
+                a - Turn Left
+                d - Turn Right
+                x - Stop All
+                u - Arm Up
+                j - Arm Down
+                v - Vibration On
+                b - Vibration Off
+                q - Quit
+                """)
             time.sleep(0.1)  # Prevent CPU overuse
 
     except KeyboardInterrupt:
